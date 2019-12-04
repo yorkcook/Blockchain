345e629ce34c2c24ec3f4443c1f8f9cfed534599
Zolton <gmzesq@gmail.com>
2019-12-04 13:35:39 -0500

    WIP

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 
 from flask import Flask, jsonify, request
 
+DIFFICULTY = 3
 
 class Blockchain(object):
     def __init__(self):
@@ -32,14 +33,21 @@ class Blockchain(object):
 
         block = {
             # TODO
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.current_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain[-1])
         }
 
         # Reset the current list of transactions
+        self.current_transactions = []
         # Append the chain to the block
+        self.chain.append(block)
         # Return the new block
-        pass
+        return block
 
-    def hash(block):
+    def hash(self, block):
         """
         Creates a SHA-256 hash of a Block
 
@@ -56,9 +64,9 @@ class Blockchain(object):
         # or we'll have inconsistent hashes
 
         # TODO: Create the block_string
-
+        block_string = json.dumps(block, sort_keys=True).encode()
         # TODO: Hash this string using sha256
-
+        hash = hashlib.sha256(block_string).hexdigest()
         # By itself, the sha256 function returns the hash in a raw string
         # that will likely include escaped characters.
         # This can be hard to read, but .hexdigest() converts the
@@ -66,7 +74,7 @@ class Blockchain(object):
         # easier to work with and understand
 
         # TODO: Return the hashed block string in hexadecimal format
-        pass
+        return hash
 
     @property
     def last_block(self):
@@ -96,9 +104,10 @@ class Blockchain(object):
         correct number of leading zeroes.
         :return: True if the resulting hash is a valid proof, False otherwise
         """
-        # TODO
-        pass
-        # return True or False
+        guess = f"{block_string}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        # return True or False - [:3] = slice off first 3 digits
+        return guess_hash[:DIFFICULTY] == "0" * DIFFICULTY
 
 
 # Instantiate our Node
@@ -128,8 +137,11 @@ def mine():
 def full_chain():
     response = {
         # TODO: Return the chain and its current length
+        "length": len(blockchain.chain),
+        "chain": blockchain.chain
     }
     return jsonify(response), 200
+
 
 
 # Run the program on port 5000
