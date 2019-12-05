29f1b556d81b44c3b3e90fd05e20a2200ba7cab1
York Cook <yorkcook@Johnny-Castle.lan1>
2019-12-05 12:31:01 -0500

    WIP

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,6 +4,7 @@ import requests
 import sys
 import json
 
+DIFFICULTY = 3
 
 def proof_of_work(block):
     """
@@ -13,7 +14,11 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +32,9 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:DIFFICULTY] == "0" * DIFFICULTY
 
 
 if __name__ == '__main__':
@@ -45,6 +52,7 @@ if __name__ == '__main__':
 
     # Run forever until interrupted
     while True:
+        print('Starting to mine')
         r = requests.get(url=node + "/last_block")
         # Handle non-json response
         try:
@@ -57,6 +65,9 @@ if __name__ == '__main__':
 
         # TODO: Get the block from `data` and use it to look for a new proof
         # new_proof = ???
+        last_block = data['last_block']
+        new_proof = proof_of_work[last_block]
+        print(f'Block mined: {new_proof}')
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
